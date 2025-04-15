07cfb0715c267d01fcd687781e7d696c922f9907
WalterHorst <whorst42@gmail.com>
2025-04-15 11:52:22 -0300

    fix: refactor CORS origin configuration to use environment variable

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,12 +4,13 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
 const PORT = process.env.PORT || 4000;
+const ORIGIN = process.env.ORIGIN || 'http://localhost:3000';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ORIGIN,
     credentials: true,
   });
 
